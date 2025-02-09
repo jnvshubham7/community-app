8de78fdd9bcffbcdc45ec351d6931818a112b828
Shubham Kumar Bhokta <69629803+jnvshubham7@users.noreply.github.com>
2025-02-09 18:04:57 +0530

    login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  email: string = '';
+  password: string = '';
+
+  constructor(private router: Router) 
+  {}
+
+  login()
+  {
+    if(this.email == 'admin' && this.password == 'admin')
+    {
+      this.router.navigate(['/community']);
+    }
+    else
+    {
+      alert('Invalid credentials');
+    }
+  }
+
 
 }
